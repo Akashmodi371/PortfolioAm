ecc01b108000d9f1dd53e931358d35e15b0da97b
aakash <akashmodi371@gmail.com>
2022-07-08 16:04:10 +0530

    uncomment rerquire

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p_d#qmh^47+-nie*)o0=k6g(vvtid)bxv3r$v81uy!u)%7+nc*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
